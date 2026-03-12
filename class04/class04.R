556246289e63e9f0a79864760b13c1c62b046ae5
JoshuaKK05 <jkkhalil@ucsd.edu>
2026-03-12 11:21:59 -0700

    Add files via upload

---
+++ class04/class04.R
@@ -0,0 +1,10 @@
+#' ---
+#' title: "Class 4 R Intro "
+#' author: "Joshua Khalil"
+#' date: "January 15th, 2026"
+#' ---
+x <- 1:50
+plot(x, sin(x))
+
+plot (x, sin(x), typ="l", col="blue", lwd=2)
+log(10, base =10 )
